2ffae9767ef71ac5282aeec1dfb2a9cf3e9ac16c
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-09-04 05:07:18 +0530

    update constants

---
+++ HandGesturesRecognition/Tensorflow/resources/constants.py
@@ -0,0 +1,3 @@
+MODEL_NAME = 'my_ssd_mobnet'
+PRETRAINED_MODEL_PATH = "HandGesturesRecognition\Tensorflow\\resources\\pre-trained-models"
+CONFIGURATION_FILE_PATH = "HandGesturesRecognition\Tensorflow\\resources\models\\" + MODEL_NAME + "\\pipeline.config"
